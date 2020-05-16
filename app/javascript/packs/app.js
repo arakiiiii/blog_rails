e3c59e02ba693ec51d5a906a5ab7001525a00caa
arakiiiii <kamiteakira@gmail.com>
2020-05-17 00:05:29 +0900

    vuex

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
+
+//vuex
+import store from '../stores';
+
 const app = new Vue({
   el: "#vue_app",
   data() {
     return {
-
+      show_menu: true,
     }
   },
   methods: {
-
+    toggleMenu() {
+      this.show_menu = !this.show_menu;
+      console.log(this.show_menu)
+    }
   }
 })
